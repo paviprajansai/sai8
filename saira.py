d13ecd184cea97ada588566ec61db63ff64e37a1
paviprajansai <52271026+paviprajansai@users.noreply.github.com>
2019-07-31 13:01:25 +0530

    Create saira.py

---
+++ saira.py
@@ -0,0 +1,22 @@
+n11=int(input())
+if n11==1:
+	print("One")
+elif n11==2:
+	print("Two")
+elif n11==3:
+	print("Three")
+elif n11==4:
+	print("Four")
+elif n11==5:
+	print("Five")
+elif n11==6:
+	print("Six")
+elif n11==7:
+	print("Seven")
+elif n11==8:
+	print("Eight")
+elif n11==9:
+	print("Nine")
+else:
+	print("Ten")
+  #hi
